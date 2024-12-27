024d00f4059393a3bc70d0782a8085caeb16fdca
paudelb36 <paudelb11@gmail.com>
2024-12-27 10:59:58 +0545

    added resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
       {/* Download Section */}
       <div className="mb-8 text-center">
         <a
-          href="#"
+          href="/assets/Bibek Paudel Resume.pdf"
           // download
           className="inline-flex items-center gap-2 bg-primary text-white px-6 py-3 rounded-lg hover:bg-primary/90 transition-colors"
         >
